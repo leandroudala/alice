3c11f031cd2f7096ff67011711d90e19bcdbe1a2
Leandro Udala <leandroudala@gmail.com>
2025-02-16 18:40:48 -0300

    chore: adding FDTExtractor to main

--- src/main/main.py
+++ src/main/main.py
@@ -1,14 +1,26 @@
 from core.application.xrf_extractor import XRFExtractor
 from core.application.mst_extractor import MSTExtractor
+from core.application.fdt_extractor import FDTExtractor
+
+database = "DOC"
+extension_upper = False
+
+
+def get_file(extension: str) -> str:
+    global extension_upper, database
+    return "data/%s.%s" % (
+        database,
+        extension.upper() if extension_upper else extension.lower(),
+    )
 
 
 cross_reference = None
-xrf_file = "data/DOC.xrf"
+xrf_file = get_file("xrf")
 
 xrf_extractor = XRFExtractor(xrf_file)
 cross_reference = xrf_extractor.to_cross_reference()
 
-mst_file = "data/DOC.mst"
+mst_file = get_file("mst")
 print(cross_reference)
 mst_extractor = MSTExtractor(mst_file)
 
@@ -16,3 +28,12 @@ for pointer in cross_reference.pointers:
     processed = mst_extractor.extract_data(pointer)
     print(processed)
     print()
+
+fdt_file = get_file("fdt")
+fdt_extractor = FDTExtractor(fdt_file)
+columns = fdt_extractor.extract_data()
+
+for column in columns:
+    print(column)
+
+print("Columns found", len(columns))
