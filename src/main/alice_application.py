03eed21928240ea0efb9d583cc40311c770f60ef
Leandro Udala <leandroudala@gmail.com>
2025-02-16 20:21:33 -0300

    feat: created friendly cli menu

--- src/main/alice_application.py
+++ src/main/alice_application.py
@@ -1,11 +1,14 @@
+import os
+import sys
+
 from core.application.fdt_extractor import FDTExtractor
 from core.application.mst_extractor import MSTExtractor
 from core.application.xrf_extractor import XRFExtractor
 from core.domain.cross_reference import CrossReference
-from core.domain.table_definition import ColumnDefinition
 from core.domain.master_file import Record
+from core.domain.table_definition import ColumnDefinition
 
-database = "THES"
+database = "CDS"
 extension_upper = True  # linux is case sensitive
 
 
@@ -16,6 +19,8 @@ def get_file(extension: str) -> str:
 
 
 def extract_cross_references() -> list[CrossReference]:
+    # Processing XRF File
+    xrf_file = get_file("xrf")
     xrf_extractor = XRFExtractor(xrf_file)
     return xrf_extractor.to_cross_reference()
 
@@ -33,23 +38,56 @@ def extract_field_table_definition() -> ColumnDefinition:
     print("Columns found", len(columns))
 
 
-# Processing XRF File
-cross_reference = None
-xrf_file = get_file("xrf")
-
-cross_references = extract_cross_references()
-
-# Processing MST File
-mst_file = get_file("mst")
-mst_extractor = MSTExtractor(mst_file)
+def extract_master_file() -> MSTExtractor:
+    # Processing MST File
+    mst_file = get_file("mst")
+    return MSTExtractor(mst_file)
 
 
-total_records: list[Record] = []
-for reference in cross_references:
-    pointers = reference.pointers
-    for pointer in pointers:
-        processed = mst_extractor.extract_data(pointer)
-        total_records.append(processed)
+def show_menu():
+    print("Escolha uma das opções abaixo:")
+    print("1 - Gerar SQLite a partir de dados do CDS/ISIS")
+    print("0 - Sair")
+    print()
+
+    return input("> ")
 
 
-print("Database: %s, records: %d" % (database, len(total_records)))
+def etl():
+    cross_references = extract_cross_references()
+    mst_extractor = extract_master_file()
+
+    total_records: list[Record] = []
+    for reference in cross_references:
+        pointers = reference.pointers
+        for pointer in pointers:
+            processed = mst_extractor.extract_data(pointer)
+            total_records.append(processed)
+
+    print("Database: %s, records: %d" % (database, len(total_records)))
+    print()
+
+
+def exit_system():
+    os.system('clear')
+    print('Encerrando...')
+    sys.exit(0)
+
+
+MENU_OPTIONS = [
+    exit_system,
+    etl
+]
+
+def run_app():
+    while True:
+        try:
+            selected = int(show_menu())
+            MENU_OPTIONS[selected]()
+        except Exception:
+            os.system('clear')
+            print("Opção desconhecida:", selected, '\n')
+
+
+if __name__ == "__main__":
+    run_app()
