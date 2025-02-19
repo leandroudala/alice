ba94f4003b450070421b077cf124cd7148ddfa0e
Leandro Udala <leandroudala@gmail.com>
2025-02-18 21:53:35 -0300

    chore: added create db option into alice's console app

--- src/main/alice_application.py
+++ src/main/alice_application.py
@@ -7,6 +7,8 @@ from domain.usecases.xrf_extractor import XRFExtractor
 from domain.entities.cross_reference import CrossReference
 from domain.entities.master_file import Record
 from domain.entities.table_definition import ColumnDefinition
+from domain.usecases.fdt_to_database import FTDToDatabase
+from infrastructure.database.sqlite_db import SQLiteDatabase
 
 database = "CDS"
 extension_upper = True  # linux is case sensitive
@@ -25,17 +27,14 @@ def extract_cross_references() -> list[CrossReference]:
     return xrf_extractor.to_cross_reference()
 
 
-def extract_field_table_definition() -> ColumnDefinition:
+def extract_field_table_definition() -> list[ColumnDefinition]:
     # Processing FDT File
     fdt_file = get_file("fdt")
 
     fdt_extractor = FDTExtractor(fdt_file)
     columns = fdt_extractor.extract_data()
 
-    for column in columns:
-        print(column)
-
-    print("Columns found", len(columns))
+    return columns
 
 
 def extract_master_file() -> MSTExtractor:
@@ -46,7 +45,8 @@ def extract_master_file() -> MSTExtractor:
 
 def show_menu():
     print("Escolha uma das opções abaixo:")
-    print("1 - Gerar SQLite a partir de dados do CDS/ISIS")
+    print("1 - Criar banco de dados")
+    print("2 - Gerar SQLite a partir de dados do CDS/ISIS")
     print("0 - Sair")
     print()
 
@@ -69,25 +69,30 @@ def etl():
 
 
 def exit_system():
-    os.system('clear')
-    print('Encerrando...')
+    os.system("clear")
+    print("Encerrando...")
     sys.exit(0)
 
 
-MENU_OPTIONS = [
-    exit_system,
-    etl
-]
+def create_db():
+    print("\nCriando tabela")
+    columns = extract_field_table_definition()
+    db = SQLiteDatabase(database)
+    db_creator = FTDToDatabase(db)
+    db_creator.create_table("cds", columns)
+
+
+MENU_OPTIONS = [exit_system, create_db, etl]
+
 
 def run_app():
     while True:
         try:
-            selected = int(show_menu())
+            value = show_menu()
+            selected = int(value)
             MENU_OPTIONS[selected]()
-        except Exception:
-            os.system('clear')
-            print("Opção desconhecida:", selected, '\n')
-
+        except ValueError:
+            print('Opção inválida:', value, '\n\n')
 
 if __name__ == "__main__":
     run_app()
